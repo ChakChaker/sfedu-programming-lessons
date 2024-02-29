b77828ce82a70b71c29bcf68dfd62b5f3cb41fef
ChakChaker <84287643+ChakChaker@users.noreply.github.com>
2024-02-29 10:46:30 +0300

    Корректировки 2 лабы

--- python/lab02/task5/2.py
+++ python/lab02/task5/2.py
@@ -1,8 +1,25 @@
 a = 1 + 2j
 b = 3 + 1j
+c = 5
+d = 7.0
 
+print('Комплексные оба:')
 print(a * b)
 print(a / b)
 print(a ** b)
 # print(a // b)
 # print(a % b)
+
+print('Комплексное и целое:')
+print(a * c)
+print(a / c)
+print(a ** c)
+# print(a // c)
+# print(a % c)
+
+print('Комплексное и вещественное:')
+print(a * d)
+print(a / d)
+print(a ** d)
+# print(a // d)
+# print(a % d)
