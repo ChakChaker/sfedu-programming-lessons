2e4a05f877959b8da40e2ad1d378759f1e855d19
ChakChaker <84287643+ChakChaker@users.noreply.github.com>
2024-04-11 10:39:14 +0300

    правки 7 лабы

--- python/lab07/palindrom1.py
+++ python/lab07/palindrom1.py
@@ -11,22 +11,21 @@ def get_word():
 
 
 def is_palindrome_old(word):
-    if word == word[::-1]:
-        return "палиндром"
-    else:
-        return "не палиндром"
+    return "палиндром" if word == word[::-1] else "не палиндром"
 
 
 def is_palindrome(word):
-    word_lower = word.lower()
-    return word_lower == word_lower[::-1]
+    return word.lower() == word.lower()[::-1]
 
 
-def create_message():
+def create_message(word, prefix):
+    print('Слово %s - %s палиндром' % (word, prefix))
+
+
+def check_palindrome():
     word = get_word()
     prefix = '' if is_palindrome(word) else 'не'
-    answer = 'Слово %s - %s палиндром' % (word, prefix)
-    print(answer)
+    create_message(word, prefix)
 
 
-create_message()
+check_palindrome()
